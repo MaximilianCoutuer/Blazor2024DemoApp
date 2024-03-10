e344f6e33b401beaefd2cd19030fa9523f7cc355
Maximilian Coutuer <maximilian.coutuer@outlook.com>
2024-03-10 13:38:00 +0100

    ComponentBasics chapter

---
+++ Blazor2024DemoApp/Blazor2024DemoApp/Components/Pages/ExampleCodeBehind.razor.cs
@@ -0,0 +1,10 @@
+﻿namespace Blazor2024DemoApp.Components.Pages
+{
+    public partial class ExampleCodeBehind
+    {
+        private int Calculate()
+        {
+            return 2;
+        }
+    }
+}
---
+++ Blazor2024DemoApp/Blazor2024DemoApp/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace Blazor2024DemoApp.Models
+{
+    public class Person
+    {
+        public string Name { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public DateTime BirthDate { get; set; } = new DateTime(2000, 1, 1);
+    }
+}
