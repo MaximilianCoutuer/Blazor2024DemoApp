e27b02a3e689698e4afa765e82458bc1fce0d067
Maximilian Coutuer <maximilian.coutuer@outlook.com>
2024-03-10 17:50:38 +0100

    Binding chapter

---
+++ Blazor2024DemoApp/Blazor2024DemoApp/Components/Exercise/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace Blazor2024DemoApp.Components.Exercise
+{
+    public class Person
+    {
+        public string Name { get; set; } = "";
+        public string FirstName { get; set; } = "";
+        public int Age { get; set; }
+        public bool IsInetumEmployee { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} {FirstName}, age {Age}";
+        }
+    }
+}
