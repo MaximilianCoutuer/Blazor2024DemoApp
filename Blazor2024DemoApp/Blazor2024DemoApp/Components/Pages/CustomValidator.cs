146233e312d24c7995cadbf3af6a69ca95ecf5d0
Maximilian Coutuer <maximilian.coutuer@outlook.com>
2024-03-11 13:49:26 +0100

    Forms chapter

---
+++ Blazor2024DemoApp/Blazor2024DemoApp/Components/Pages/CustomValidator.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Numerics;
+using System.Xml.Linq;
+
+namespace Blazor2024DemoApp.Components.Pages
+{
+    public class NameValidator : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(
+            object? value,
+            ValidationContext validationContext)
+        {
+            if (value == null)
+            {
+                return new ValidationResult("Name is blank");
+            }
+            var name = value.ToString();
+            return (name.StartsWith("A")
+                ? null
+                : new ValidationResult("Must start with A"));
+        }
+    }
+}
---
+++ Blazor2024DemoApp/Blazor2024DemoApp/Components/Pages/Person.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blazor2024DemoApp.Components.Pages
+{
+    public class Person
+    {
+        [NameValidator]
+        public string Name { get; set; }
+        [Required]
+        public int Age { get; set; }
+        public DateTime Birthday { get; set;}
+        public int City { get; set;}
+        public string Gender { get; set;}
+    }
+}
